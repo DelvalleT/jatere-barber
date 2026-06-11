f0cc3575076355b59952870135a13f1bca909ee3
Tobias Delvalle <torobias18@gmail.com>
2026-06-10 21:17:02 -0300

    fix: usar DATABASE_URL interna de Railway

--- db.js
+++ db.js
@@ -11,6 +11,7 @@ console.log('🔌 Conectando a:', connString?.slice(0, 50) + '...');
 
 const pool = new Pool({
   connectionString: connString,
+  ssl: { rejectUnauthorized: false },
   family: 4,
   connectionTimeoutMillis: 10000,
 });
